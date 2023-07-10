418c32c671a0e256e222424e9180cacf8d67cb3b
yeyeye <h2012497638@outlook.com>
2023-07-10 18:35:50 +0800

    ✨ feat: 添加内存检测功能

--- source/loader/loader.h
+++ source/loader/loader.h
@@ -1,4 +1,17 @@
 #ifndef LOADER_H
 #define LOADER_H
 
+#include "comm/types.h"
+#include "comm/boot_info.h"
+
+// 内存检测信息结构
+typedef struct SMAP_entry {
+    uint32_t BaseL; // base address uint64_t
+    uint32_t BaseH;
+    uint32_t LengthL; // length uint64_t
+    uint32_t LengthH;
+    uint32_t Type; // entry Type
+    uint32_t ACPI; // extended
+}__attribute__((packed)) SMAP_entry_t;
+
 #endif
--- source/loader/loader_16.c
+++ source/loader/loader_16.c
@@ -3,7 +3,11 @@
  */
 __asm__(".code16gcc");
 
-void show_msg(char* msg)
+#include "loader.h"
+
+static boot_info_t boot_info;
+
+static void show_msg(char* msg)
 {
     char c;
     while((c = *msg++) != '\0'){
@@ -13,6 +17,47 @@ void show_msg(char* msg)
             "int $0x10"::[ch]"r"(c)
         );
     }
+}
+
+static void  detect_memory(void) {
+	uint32_t contID = 0;
+	SMAP_entry_t smap_entry;
+	int signature, bytes;
+
+    show_msg("try to detect memory:");
+
+	// 初次：EDX=0x534D4150,EAX=0xE820,ECX=24,INT 0x15, EBX=0（初次）
+	// 后续：EAX=0xE820,ECX=24,
+	// 结束判断：EBX=0
+	boot_info.ram_region_count = 0;
+	for (int i = 0; i < BOOT_RAM_REGION_MAX; i++) {
+		SMAP_entry_t * entry = &smap_entry;
+
+		__asm__ __volatile__("int  $0x15"
+			: "=a"(signature), "=c"(bytes), "=b"(contID)
+			: "a"(0xE820), "b"(contID), "c"(24), "d"(0x534D4150), "D"(entry));
+		if (signature != 0x534D4150) {
+            show_msg("failed.\r\n");
+			return;
+		}
+
+		// todo: 20字节
+		if (bytes > 20 && (entry->ACPI & 0x0001) == 0){
+			continue;
+		}
+
+        // 保存RAM信息，只取32位，空间有限无需考虑更大容量的情况
+        if (entry->Type == 1) {
+            boot_info.ram_region_cfg[boot_info.ram_region_count].start = entry->BaseL;
+            boot_info.ram_region_cfg[boot_info.ram_region_count].size = entry->LengthL;
+            boot_info.ram_region_count++;
+        }
+
+		if (contID == 0) {
+			break;
+		}
+	}
+    show_msg("ok.\r\n");
 }
 
 
