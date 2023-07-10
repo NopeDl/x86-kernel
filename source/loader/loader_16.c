5a80e903cd173889b342fd61ea5ca215ab5030b1
yeyeye <h2012497638@outlook.com>
2023-07-10 16:55:01 +0800

    ✨ feat: 添加输出
    c内联输出

--- source/loader/loader_16.c
+++ source/loader/loader_16.c
@@ -3,8 +3,22 @@
  */
 __asm__(".code16gcc");
 
+void show_msg(char* msg)
+{
+    char c;
+    while((c = *msg++) != '\0'){
+        __asm__ __volatile__(
+            "mov $0xe, %%ah\n\t"
+            "mov %[ch], %%al\n\t"
+            "int $0x10"::[ch]"r"(c)
+        );
+    }
+}
+
+
 void loader_entry(void)
 {
+    show_msg("hello,world");
     for (;;)
     {
     }
