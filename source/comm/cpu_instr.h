7c776ffcab03ed287f9fab7df587c71fdc9fc488
yeyeye <h2012497638@outlook.com>
2023-07-11 01:33:27 +0800

    ✨ feat: 打开保护模式
    已实现关中断、打开A20地址线、加载GDT表相关内联汇编函数和调用

--- source/comm/cpu_instr.h
+++ source/comm/cpu_instr.h
@@ -1,6 +1,8 @@
 #ifndef CPU_INSTR_H
 #define CPU_INSTR_H
 
+#include "types.h"
+
 static inline void cli(void)
 {
     __asm__ __volatile__("cli");
@@ -9,6 +11,35 @@ static inline void cli(void)
 static inline void sti(void)
 {
     __asm__ __volatile__("sti");
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    __asm__ __volatile__("inb %[p], %[v]"
+                         : [v] "=a"(ret)
+                         : [p] "d"(port));
+    return ret;
+}
+
+static inline void outb(uint16_t port, uint8_t data)
+{
+    __asm__ __volatile__("outb %[v], %[p]" ::[v] "a"(data), [p] "d"(port));
+}
+
+static inline void lgdt(uint32_t begin, uint32_t size)
+{
+    struct
+    {
+        uint16_t limit;
+        uint32_t start15_0;
+        uint32_t start31_16;
+    }gdt;
+    gdt.start31_16 = begin >> 16;
+    gdt.start15_0 = begin & 0xffff;
+    gdt.limit = size - 1;
+
+    __asm__ __volatile__("lgdt %[g]"::[g]"m"(gdt));
 }
 
 #endif
