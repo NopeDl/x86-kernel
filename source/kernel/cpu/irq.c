a72fead46a3a516103c5af05c403e59bb64913ff
yeyeye <h2012497638@outlook.com>
2023-07-16 15:32:23 +0800

    ✨ feat: 汇编宏

--- source/kernel/cpu/irq.c
+++ source/kernel/cpu/irq.c
@@ -7,21 +7,22 @@
 
 static gate_desc_t idt_table[IDT_TABLE_NR];
 
-void exception_handle_unknown(void);
-
-
-static void do_default_handle(excption_frame_t* frame, const char * msg)
+static void do_default_handle(excption_frame_t *frame, const char *msg)
 {
     while (1)
     {
         /* code */
     }
-    
 }
 
-void do_handle_unknown(excption_frame_t* frame)
+void do_handle_unknown(excption_frame_t *frame)
 {
     do_default_handle(frame, "unknown exception");
+}
+
+void do_handle_divider(excption_frame_t *frame)
+{
+    do_default_handle(frame, "divider exception");
 }
 
 void irq_init(void)
@@ -29,8 +30,21 @@ void irq_init(void)
     for (int i = 0; i < IDT_TABLE_NR; i++)
     {
         gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t)exception_handle_unknown,
-        GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_INT);
+                      GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_INT);
     }
 
+    irq_install(IRQ0_DE, exception_handle_divider);
+
     lidt((uint32_t)idt_table, sizeof(idt_table));
+}
+
+void irq_install(int num, exception_handle_t handle)
+{
+    if (num >= IDT_TABLE_NR)
+    {
+        return;
+    }
+    
+    gate_desc_set(idt_table + num, KERNEL_SELECTOR_CS, (uint32_t)handle,
+                      GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_INT);
 }
--- source/kernel/include/cpu/irq.h
+++ source/kernel/include/cpu/irq.h
@@ -2,16 +2,23 @@
 #define IRQ_H
 #include "comm/types.h"
 
+#define IRQ0_DE     0
+
 typedef struct 
 {
     uint32_t gs, fs, es, ds;
     uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uint32_t num, code;
     uint32_t eip, cs, eflags;
 }excption_frame_t;
 
-
+typedef void (*exception_handle_t)(void);
 
 void irq_init();
+void irq_install(int num, exception_handle_t handle);
+void exception_handle_unknown(void);
+void exception_handle_divider(void);
+
 
 
 #endif
