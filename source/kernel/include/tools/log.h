37ac0cd996a06841bceebcda34959477f5f0c3e0
yeyeye <h2012497638@outlook.com>
2023-07-17 17:47:28 +0800

    ✨ feat: log init

--- source/kernel/include/tools/log.h
+++ source/kernel/include/tools/log.h
@@ -1,6 +1,8 @@
 #ifndef LOG_H
 #define LOG_H
 
+#define COM1_PORT   0x3F8
+
 void log_init();
 void log_printf(const char* msg, ...);
 
--- source/kernel/tools/log.c
+++ source/kernel/tools/log.c
@@ -1,8 +1,18 @@
 #include "tools/log.h"
+#include "comm/cpu_instr.h"
 
+//暂时使用串行接口方法
 void log_init()
 {
-
+    //清除串行接口自身中断
+    outb(COM1_PORT + 1, 0x00);
+    //配置
+    outb(COM1_PORT + 3, 0x80);
+    outb(COM1_PORT + 0, 0x03);
+    outb(COM1_PORT + 1, 0x00);
+    outb(COM1_PORT + 3, 0x03);
+    outb(COM1_PORT + 2, 0xc7);
+    outb(COM1_PORT + 4, 0x0f);
 }
 
 
