ebb0febcba55c02d1c82aa3513eb9fe11e527019
yeyeye <h2012497638@outlook.com>
2023-07-19 20:13:47 +0800

    ✨ feat: list delete method

--- source/kernel/tools/list.c
+++ source/kernel/tools/list.c
@@ -24,7 +24,7 @@ void list_insert_first(list_t *list, list_node_t *node)
 
 void list_insert_last(list_t *list, list_node_t *node)
 {
-    node.pre = list->last;
+    node->pre = list->last;
     node->next = (list_node_t *)0;
     if (list_is_empty(list))
     {
@@ -36,4 +36,52 @@ void list_insert_last(list_t *list, list_node_t *node)
         list->last = node;
     }
     list->count++;
+}
+
+list_node_t *list_remove_first(list_t *list)
+{
+    if (list_is_empty(list))
+    {
+        return (list_node_t *)0;
+    }
+    list_node_t *t = list->first;
+    list->first = t->next;
+    if (list->first == (list_node_t *)0)
+    {
+        list->last = (list_node_t *)0;
+    }
+    else
+    {
+        t->next->pre = (list_node_t *)0;
+    }
+    list->count--;
+
+    t->next = t->pre = (list_node_t *)0;
+    return t;
+}
+
+list_node_t *list_remove(list_t *list, list_node_t *node)
+{
+    if (node == list->first)
+    {
+        list->first = node->next;
+    }
+
+    if (node == list->last)
+    {
+        list->last = node->pre;
+    }
+
+    if (node->pre)
+    {
+        node->pre->next = node->next;
+    }
+
+    if (node->next)
+    {
+        node->next->pre = node->pre;
+    }
+
+    node->pre = node->next = (list_node_t *)0;
+    return node;
 }
