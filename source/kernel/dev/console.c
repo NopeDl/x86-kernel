783e6f5bc297b533aba83defcdfbcbc13971d560
yeyeye <h2012497638@outlook.com>
2023-08-08 15:06:16 +0800

    ✨ feat: add '\n'

--- source/kernel/dev/console.c
+++ source/kernel/dev/console.c
@@ -1,7 +1,38 @@
 #include "dev/concole.h"
+#include "tools/klib.h"
 
 #define CONSOLE_NR 1
 static console_t console_buf[CONSOLE_NR];
+
+/**
+ * 擦除line行
+ */
+static void erase_rows(console_t* console, int start, int end)
+{
+    disp_char_t* disp_start = console->disp_base + console->disp_cols * start;
+    disp_char_t* disp_end = console->disp_base + console->disp_cols * (end + 1);
+    while (disp_start < disp_end) {
+        disp_start->c = ' ';
+        disp_start->foreground = console->foreground;
+        disp_start->background = console->background;
+        disp_start++;
+    }
+}
+
+/**
+ * 上滚
+ */
+static void scroll_up(console_t* console, int line)
+{
+    // 上移一行
+    disp_char_t* dest = console->disp_base;
+    disp_char_t* src = console->disp_base + console->disp_cols * line;
+    uint32_t size = (console->disp_row - line) * console->disp_cols * sizeof(disp_char_t);
+    kernel_memcpy((void*)dest, (void*)src, size);
+    // 清空最后line行
+    erase_rows(console, console->disp_row - line, console->disp_row - 1);
+    console->cursor_row -= line;
+}
 
 static void move_cursor(console_t* console, int step)
 {
@@ -9,6 +40,10 @@ static void move_cursor(console_t* console, int step)
         if (++console->cursor_col >= console->disp_cols) {
             console->cursor_row++;
             console->cursor_col = 0;
+
+            if (console->cursor_row >= console->disp_row) {
+                scroll_up(console, 1);
+            }
         }
     }
 }
@@ -23,6 +58,33 @@ static void show_char(console_t* console, char c)
     move_cursor(console, 1);
 }
 
+static void move_to_col0(console_t* console)
+{
+    console->cursor_col = 0;
+}
+
+static void move_next_line(console_t* console)
+{
+    console->cursor_row++;
+    if (console->cursor_row >= console->disp_row) {
+        scroll_up(console, 1);
+    }
+}
+
+/**
+ * 清屏
+ */
+static void clear_display(console_t* console)
+{
+    int size = console->disp_cols * console->disp_row;
+    disp_char_t* start = console->disp_base;
+    for (int i = 0; i < size; i++, start++) {
+        start->c = ' ';
+        start->foreground = console->foreground;
+        start->background = console->background;
+    }
+}
+
 int console_init()
 {
     for (int i = 0; i < CONSOLE_NR; i++) {
@@ -32,6 +94,8 @@ int console_init()
         console_buf[i].disp_cols = CONSOLE_COL_MAX;
         console_buf[i].disp_row = CONSOLE_ROW_MAX;
         console_buf[i].disp_base = (disp_char_t*)CONSOLE_DISP_ADDR + i * (CONSOLE_COL_MAX * CONSOLE_ROW_MAX);
+
+        clear_display(&console_buf[i]);
     }
     return 0;
 }
@@ -43,8 +107,16 @@ int console_write(int console, char* data, int size)
 
     for (len = 0; len < size; len++) {
         char ch = *data++;
+        switch (ch) {
+        case '\n':
+            move_to_col0(c);
+            move_next_line(c);
+            break;
 
-        show_char(c, ch);
+        default:
+            show_char(c, ch);
+            break;
+        }
     }
     return len;
 }
